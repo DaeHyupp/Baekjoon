4e39a6b7b2707998575a49cdec087592c848fbe4
YoonSeongBin <9603226407@daum.net>
2021-08-11 17:30:05 +0900

    N&M-2(backtracking)

---
+++ 15650.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().rstrip().split(" "))
+
+arr = []
+
+def dfs():
+    if len(arr) == M : print(' '.join(map(str,arr)));return
+
+    for i in range(1,N+1):
+        if len(arr)==0: arr.append(i)
+        else:
+            if i > arr[-1]: arr.append(i)
+            else: continue
+        dfs()
+        arr.pop()
+dfs()
+
